9858eea9d1cd3ec7571fb8c48a9a956766f8fd20
Наиль Гимаев <gimntut@list.ru>
2019-06-03 16:33:50 +0500

    xxxx

--- index.php
+++ index.php
@@ -7,6 +7,19 @@
 <body>
   <a href="/registr">Регистрация</a>
   <a href="/card">Корзина</a>
-  <?php echo "Hi!" ?>
+  <?php 
+    $host = '127.0.0.1';
+    $db   = 'magazin';
+    $user = 'root';
+    $pass = 'o4oVypiU6W0se';
+    $charset = 'utf8';
+    $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+    $opt = [
+        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES   => false,
+    ];
+    $pdo = new PDO($dsn, $user, $pass, $opt);
+  ?>
 </body>
 </html>
